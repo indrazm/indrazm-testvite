e1e3a842a0cd54ce2270e327233e3a69603b00b7
Indra Zulfi Mushoddaq <114550938+indrazm@users.noreply.github.com>
2023-06-24 10:48:02 +0700

    Update

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,1 +1,9 @@
-console.log("Hey Main.js");
+import Typed from "typed.js";
+
+const typed = new Typed("#typed", {
+  strings: ["creating products", "creating websites", "doing code"],
+  typeSpeed: 100,
+  backSpeed: 50,
+  loop: true,
+  smartBackspace: true,
+});
